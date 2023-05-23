1a32bbdbd7d7928c88f302e90ee7ded8e682b506
agelorfontana <angelorfontana@gmail.com>
2023-05-23 18:37:15 -0300

    alteração de hoje

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function idioma(params) {
+  var div = document.getElementById("descr");
+  var titulo = "titulo";
+  var subtitulo = "subtitulo";
+  div.innerText = "<h1>" + titulo +"</h1>"+ "\n" + subtitulo;
+}
+
