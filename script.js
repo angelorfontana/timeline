dce9aebd8a0503bacb05838b48578fcf519ef3c9
angelo r fontana <angelorfontana@outlook.com>
2023-05-26 15:39:10 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,76 @@
 
-function idioma(params) {
-  var div = document.getElementById("descr");
-  var titulo = "tituleeeo";
-  var subtitulo = "subtitulo";
-  document.getElementById('descr').innerHTML = "<h1>" + titulo +"</h1>"+ "\n" + subtitulo;
-    if  (params != "P"){
-      alert("oi");
-    }
+function idioma(i) {
+  if(i=="P")  
+  {
+    document.getElementById('hello').innerHTML= "Olá<span>!</span>";
+    document.getElementById('hello2').innerHTML = "Quem eu sou e o que eu faço";
+    
+    
+      document.getElementById('about').innerHTML = `
+        <p>Sou proativo, colaborativo, tenho muita vontade e facilidade de aprender, sou criativo, 
+        tenho ótima capacidade analítica, apaixonado por tecnologia e processos, 
+        tenho muita facilidade de interagir com pessoas, e por consequência aprimorada visão holística,
+         essas características aumentam muito minha capacidade de usar a tecnologia para entender 
+         e melhorar os negócios.
+        </p>
+        <p>Meu conhecimento é baseado nos fundamentos da ciência da computação e gestão de negócios, 
+        por isso absorvo novas habilidades tecnológicas com facilidade. Atualmente acumulo algumas 
+        funções na startup Play Sistemas Inteligentes, alem de coordenar e idealizar escopo e 
+        arquitetura de software do projeto, sou o principal analista e programador. As SKILLs 
+        utilizadas neste projeto foram: ASPNetCor, .Net serviço windows,NetCor Console, C#, MVC,
+         Design Patterns, bootstrap, HTML, CSS, SqlServer, IIS, DEVOPs, C, IOT, Eletronica,
+          IA-Algoritimo genetico, IA-Algoritimo PSO, IA-Machine learne reconhecimento de imagens e Pyton. 
+        </p>
+        <p>Tive a oportunidade de desenvolver e/ou coordenar projetos de tecnologia para varejo e
+         indústria de grande porte, acumulando conhecimentos em diversas áreas, tais como: comercial, 
+         contábil, controladoria, custos, finanças, gestão da qualidade, gestã de pessoas, logística 
+         interna, logística externa e processos produtivos.
+        </p>`;
 
+    /*                <p>I'm proactive, collaborative, I'm very willing and easy to learn, I'm passionate about technology and processes, I have an extreme ease of interacting with people and, consequently, a great olistic vision, all these characteristics greatly increase my ability to use technology to improve business.</p>
+                <p>My knowledge is based on the fundamentals of computer science, so I absorb new technological skills with ease.
+                    Particularly new technologies or business models are enjoyable challenges for me. </p>
+                <p>I had the opportunity to develop and/or coordinate technology projects for retail and industry, accumulating knowledge in several areas, such as:
+                    commercial, accounting, controllership, costs, finance, quality management, internal logistics, external logistics and production processes. 
+ */
+  }
+  
+
+
+
+  if(i=="E")  
+  {
+      document.getElementById('hello').innerHTML = "Hello<span>!</span>";
+      document.getElementById('hello2').innerHTML = "Here's who I am & what I do";
+      
+      
+        document.getElementById('about').innerHTML = `
+          <p>Sou proativo, colaborativo, tenho muita vontade e facilidade de aprender, sou criativo, 
+          tenho ótima capacidade analítica, apaixonado por tecnologia e processos, 
+          tenho muita facilidade de interagir com pessoas, e por consequência aprimorada visão holística,
+           essas características aumentam muito minha capacidade de usar a tecnologia para entender 
+           e melhorar os negócios.
+          </p>
+          <p>Meu conhecimento é baseado nos fundamentos da ciência da computação e gestão de negócios, 
+          por isso absorvo novas habilidades tecnológicas com facilidade. Atualmente acumulo algumas 
+          funções na startup Play Sistemas Inteligentes, alem de coordenar e idealizar escopo e 
+          arquitetura de software do projeto, sou o principal analista e programador. As SKILLs 
+          utilizadas neste projeto foram: ASPNetCor, .Net serviço windows,NetCor Console, C#, MVC,
+           Design Patterns, bootstrap, HTML, CSS, SqlServer, IIS, DEVOPs, C, IOT, Eletronica,
+            IA-Algoritimo genetico, IA-Algoritimo PSO, IA-Machine learne reconhecimento de imagens e Pyton. 
+          </p>
+          <p>Tive a oportunidade de desenvolver e/ou coordenar projetos de tecnologia para varejo e
+           indústria de grande porte, acumulando conhecimentos em diversas áreas, tais como: comercial, 
+           contábil, controladoria, custos, finanças, gestão da qualidade, gestã de pessoas, logística 
+           interna, logística externa e processos produtivos.
+          </p>`;
+  
+      /*                <p>I'm proactive, collaborative, I'm very willing and easy to learn, I'm passionate about technology and processes, I have an extreme ease of interacting with people and, consequently, a great olistic vision, all these characteristics greatly increase my ability to use technology to improve business.</p>
+                  <p>My knowledge is based on the fundamentals of computer science, so I absorb new technological skills with ease.
+                      Particularly new technologies or business models are enjoyable challenges for me. </p>
+                  <p>I had the opportunity to develop and/or coordinate technology projects for retail and industry, accumulating knowledge in several areas, such as:
+                      commercial, accounting, controllership, costs, finance, quality management, internal logistics, external logistics and production processes. 
+   */
+    }
 }
 
